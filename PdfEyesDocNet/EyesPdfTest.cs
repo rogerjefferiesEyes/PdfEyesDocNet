15064ae6ab996b99fa6022c02a8aea52f38e7db2
Roger Jefferies <roger.jefferies@applitools.com>
2022-07-13 22:37:34 -0400

    initial commit

---
+++ PdfEyesDocNet/EyesPdfTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using Docnet.Core;
+using Docnet.Core.Models;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using Applitools.Images;
+using System.IO;
+
+namespace PdfEyesDocNet
+{
+    public class Tests
+    {
+        private Eyes eyes;
+        private string pdfFileName = "workshop-info-anand-bagmar.pdf";
+
+        [SetUp]
+        public void Setup()
+        {
+            eyes = new Eyes();
+        }
+
+        [Test]
+        public void EyesPdfTest()
+        {
+            eyes.Open("PdfEyesDocNet", pdfFileName);
+            using var docReader = DocLib.Instance.GetDocReader(
+            pdfFileName,
+            new PageDimensions(1080, 1920));
+
+            for(int i = 0; i < docReader.GetPageCount(); i++)
+            {
+                using var pageReader = docReader.GetPageReader(i);
+
+                var rawBytes = pageReader.GetImage();
+
+                var width = pageReader.GetPageWidth();
+                var height = pageReader.GetPageHeight();
+
+                // here you are taking a byte array that 
+                // represents the pixels directly where as Image.Load<Bgra32>()
+                // is expected for an image encoded in png, jpeg etc format
+                using var img = Image.LoadPixelData<Bgra32>(rawBytes, width, height);
+                // you are likely going to want this as well
+                // otherwise you might end up with transparent parts.
+                img.Mutate(x => x.BackgroundColor(Color.White));
+
+                using var ms = new MemoryStream();
+                img.SaveAsPng(ms);
+                eyes.CheckImage(ms.ToArray(), "Page - " + (i + 1));
+            }
+            eyes.Close();
+
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            eyes.AbortIfNotClosed();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+﻿## PdfEyesDocNet
+
+This is a sample .NET/C# project for testing PDF files using [Applitools Eyes](https://applitools.com/blog/visual-testing/); an AI-powered Visual Testing Platform. All dependencies have permissive open source licensing (MIT or Apache 2.0), and are available via Nuget.
+
+### Dependencies
+**DocNet.Core** - PDF processing package which makes use of Google's Pdfium native/unmanaged library.
+[![NuGet version (Docnet.Core)](https://img.shields.io/nuget/v/Docnet.Core.svg?style=flat-square)](https://www.nuget.org/packages/Docnet.Core/)
+
+**SixLabors.ImageSharp** - Image processing package offering a pure .NET/managed, cross-platform replacement for System.Drawing.
+[![NuGet version (SixLabors.ImageSharp)](https://img.shields.io/nuget/v/SixLabors.ImageSharp.svg?style=flat-square)](https://www.nuget.org/packages/SixLabors.ImageSharp/)
